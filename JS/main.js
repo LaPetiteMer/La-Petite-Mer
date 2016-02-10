0f5b3f3f44b253ccf618e2448bc3777f1d748bcb
goemaere <goemaere.leo@hotmail.fr>
2016-02-10 23:29:17 +0100

    animation menu on click

---
+++ JS/main.js
@@ -0,0 +1,32 @@
+window.addEventListener("load", function(e){
+
+	var burgerMenu = document.getElementById("burger_menu");
+	var menu = document.getElementById("menu");
+	var open = false;
+
+	var openMenu = function(){
+		menu.classList.remove('closeMenu');
+		menu.classList.add('openMenu');
+	};
+
+	var closeMenu = function(){
+		menu.classList.remove('openMenu');
+		menu.classList.add('closeMenu');
+	};
+
+	var playMenu = function(){
+			if(open === false) {
+				openMenu();
+				open = true;
+			}else{
+				closeMenu();
+				open = false;
+			}
+	};
+
+	burgerMenu.addEventListener("click", function(){
+		playMenu();
+	});
+
+
+}, false);
