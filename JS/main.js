fa75eb9ad95f3439dafb85409a8d6f6bcce0173b
goemaere <goemaere.leo@hotmail.fr>
2016-02-12 00:02:39 +0100

    animation burger menu

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,14 @@ window.addEventListener("load", function(e){
 
 	var burgerMenu = document.getElementById("burger_menu");
 	var menu = document.getElementById("menu");
-	
-// SLIDEMENU 
+	var burgerTop = document.getElementById("top");
+	var burgerMiddle = document.getElementById("middle");
+	var burgerBottom = document.getElementById("bottom");
+
 	var open = false;
+
+// SLIDEMENU 
+
 	var openMenu = function(){
 		menu.classList.remove('closeMenu');
 		menu.classList.add('openMenu');
@@ -25,14 +30,54 @@ window.addEventListener("load", function(e){
 			}
 	};
 
+	// ANIMATION BURGERMENU
+
+	var animBurgerTop = function(){
+		if(open === true){
+			burgerTop.style.animationName = "slideDown, rotateRight";	
+			burgerTop.style.animationDelay = "0s, 0.2s";	
+			burgerTop.style.animationDuration = "0.2s, 0.2s";
+			burgerTop.style.animationFillMode = "forwards, forwards";
+		}else{
+			burgerTop.style.animationName = "slideDownInverse, rotateRightInverse";	
+			burgerTop.style.animationDelay = "0.2s, 0s";	
+			burgerTop.style.animationDuration = "0.2s, 0.2s";
+			burgerTop.style.animationFillMode = "backwards, backwards";
+		}
+	}
+
+	var animBurgerBottom = function(){
+		if(open === true){
+			burgerBottom.style.animationName = "slideUp, rotateLeft";	
+			burgerBottom.style.animationDelay = "0s, 0.2s";	
+			burgerBottom.style.animationDuration = "0.2s, 0.2s";
+			burgerBottom.style.animationFillMode = "forwards, forwards";
+		}else{
+			burgerBottom.style.animationName = "slideUpInverse, rotateLeftInverse";	
+			burgerBottom.style.animationDelay = "0.2s, 0s";	
+			burgerBottom.style.animationDuration = "0.2s, 0.2s";
+			burgerBottom.style.animationFillMode = "backwards, backwards";
+		}
+	}
+
+	var displayMiddle = function(){
+		window.setTimeout(function(){
+			if(open === true){
+				burgerMiddle.style.display = "none";
+			}else {
+				burgerMiddle.style.display = "block";
+			}
+		}, 200);
+	}
+
+	// END SLIDEMENU 
+
 	burgerMenu.addEventListener("click", function(){
 		playMenu();
+		animBurgerTop();
+		animBurgerBottom();
+		displayMiddle();
 	});
-
-// END SLIDEMENU 
-
-
-// ANIMATION BURGERMENU
 
 
 
