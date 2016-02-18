bc8ef380dc292bc4613c68d4cca77d2008e9e85a
goemaere <goemaere.leo@hotmail.fr>
2016-02-18 22:26:54 +0100

    script for prices mobile

--- JS/main.js
+++ JS/main.js
@@ -97,6 +97,7 @@ window.addEventListener("load", function(e){
 		});
 	}
 
+
 	//CLOSE MENU WITH ESCAPE
 
 	document.addEventListener("keydown", function(e){
@@ -147,8 +148,68 @@ window.addEventListener("load", function(e){
 		window.setTimeout(function(){
 			initSlick();
 		}, 1000)
-		
 	}
 
+	//PRICES 
+
+	var descentList = document.getElementById('descentList');
+	var personList = document.getElementById('personList');
+	var contentPrices = document.getElementById('contentPrices');
+
+	var adultsPrices = function(){
+	    var x = descentList.value;
+	    var y = personList.value;
+	    if(x === "d1" && y == "p1"){
+		    contentPrices.innerHTML = "<p>16&#128;</p>";
+		}else if(x === "d2" && y == "p1"){
+		    contentPrices.innerHTML = "<p>20&#128;</p>";
+		}else if(x === "d3" && y == "p1"){
+		    contentPrices.innerHTML = "<p>24&#128;</p>";
+		}else if(x === "d4" && y == "p1"){
+		    contentPrices.innerHTML = "<p>35&#128;</p>";
+		}else if(x === "d5" && y == "p1"){
+		    contentPrices.innerHTML = "<p>34&#128;</p>";
+		}
+	}
+
+	var childsPrices = function(){
+	    var x = descentList.value;
+	    var y = personList.value;
+	    if(x === "d1" && y == "p2"){
+	        contentPrices.innerHTML = "<p>8&#128;</p>";
+	    }else if(x === "d2" && y == "p2"){
+	        contentPrices.innerHTML = "<p>10&#128;</p>";
+	    }else if(x === "d3" && y == "p2"){
+	        contentPrices.innerHTML = "<p>12&#128;</p>";
+	    }else if(x === "d4" && y == "p2"){
+	        contentPrices.innerHTML = "<p>17.5&#128;</p>";
+	    }else if(x === "d5" && y == "p2"){
+	        contentPrices.innerHTML = "<p>17&#128;</p>";
+	    }else if(x === "d1" && y == "p3"){
+	        contentPrices.innerHTML = "<p>14&#128;</p>";
+	    }else if(x === "d2" && y == "p3"){
+	        contentPrices.innerHTML = "<p>18&#128;</p>";
+	    }else if(x === "d3" && y == "p3"){
+	        contentPrices.innerHTML = "<p>20&#128;</p>";
+	    }else if(x === "d4" && y == "p3"){
+	        contentPrices.innerHTML = "<p>30&#128;</p>";
+	    }else if(x === "d5" && y == "p3"){
+	        contentPrices.innerHTML = "<p>29&#128;</p>";
+	    }
+	}
+
+	var descentChange = function(){
+	    adultsPrices();
+	    childsPrices();
+	} 
+
+	var personChange = function(){
+	    adultsPrices();
+	    childsPrices();
+	} 
+
+	descentList.onchange = function(){descentChange()};
+	personList.onchange = function(){personChange()};
+	
 
 }, false);
